3205e08b89cd82d16c76df8a300d22b75269fdeb
Carlos <jjbbpe@gmail.com>
2020-08-25 14:24:17 +0200

    g

--- script.js
+++ script.js
@@ -10,19 +10,21 @@ function tabla(numero){
     const multi = 'multi' + numero;
     const multiID = '#'+multi;
 
-     document.querySelector('#contenedor').innerHTML += `
-      <table id="${tablaID}">
-          <h3>Tabla del  ${numero}</h3>
-          <tbody id='${multi}'>
-          </tbody>
-      </table>
+     document.querySelector('#contenidoTablas').innerHTML += `
+
+        <div id="${tablaID}" class="tabla">
+            <h3>X  ${numero}</h3>
+            <ul id='${multi}' class="multiplicacion">
+            </ul>
+        </div>
+
       `
-     for(let i=1; i<=10; i++){
-          document.querySelector(multiID).innerHTML += ` <tr> <td >${numero} x ${i} = ${i*numero} </td></tr> `
+     for(let i=1; i<=12; i++){
+          document.querySelector(multiID).innerHTML += ` <li >${numero} x ${i} = ${i*numero} </li> `
        }
 }
 
-for(let i=1; i<=10; i++){
+for(let i=1; i<=12; i++){
      tabla(i)
   }
 
